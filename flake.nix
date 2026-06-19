612e2af00df5cbef42ef862ce82acf75a62dc3d9
NONPLAYT <76615486+NONPLAYT@users.noreply.github.com>
2026-06-19 00:10:06 +0000

    chore(release): bump version to 2.0.4

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
 
         nyx = rustPlatform.buildRustPackage {
           pname = "nyx";
-          version = "2.0.3";
+          version = "2.0.4";
 
           src = pkgs.lib.cleanSource ./.;
 
